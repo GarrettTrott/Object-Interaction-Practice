28ffcacbb9bf71cfa4762ad37ef74acf0459f0b3
Garrett Trott <garretttrott@gmail.com>
2019-12-27 16:08:23 -0800

    add checkOut and returnBook methods

--- Patron.js
+++ Patron.js
@@ -4,4 +4,18 @@ class Patron {
     this.email = email;
     this.currentBook = null;
   }
+
+  checkOut(Book) {
+    this.currentBook = Book;
+    Book.out = true;
+    Book.patron = this;
+    Book.dueDate = new Date(Date.now() + 12096e5);
+  }
+
+  returnBook(Book) {
+    this.currentBook = null;
+    Book.out = false;
+    Book.patron = null;
+    Book.dueDate = null;
+  }
 }
